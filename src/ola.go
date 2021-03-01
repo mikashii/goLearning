86062121ba898194502e089b63d70af09a9a63fe
mikashii <carolina.portas@outlook.com>
2021-02-28 22:36:27 -0300

    Initial commit only

---
+++ src/ola.go
@@ -0,0 +1,16 @@
+package main
+
+import ("fmt"
+ "os" 
+ "strconv")
+
+func main() {
+	fmt.Println("Hello carai")
+}
+
+//Only bool values can be verified through IF statements. They can be variables or functions. But both must return true or false
+//Go have two types of data list, arrays and slices.
+/*
+arrays are size limited, like Java or C
+slice can grow unlimited
+*/
